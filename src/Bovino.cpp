419d71b34beba62981400a3b14d598bc0e6311e5
JotapeFerraz <joaopedrosoaresferraz@gmail.com>
2026-06-10 22:17:52 -0300

    feat: adiciona métodos de saude e vacinacao a bovino.cpp

--- src/Bovino.cpp
+++ src/Bovino.cpp
@@ -1,7 +1,7 @@
 #include "../include/Bovino.h"
 
 #include <iostream>
-
+#include <stdexcept>
 
 
 
@@ -18,4 +18,28 @@ void Bovino::adicionarPeso(const RegistroPeso& p){
         throw std::runtime_error("Erro: Este boi está morto");
     }
     this->pesos.push_back(p);
+}
+
+void Bovino::adicionarVacina(const RegistroVacina& vacina) {
+
+    if(!this->isVivo()){
+        throw std::runtime_error("Erro: Não é possível aplicar vacina em um bovino morto.");
+    }
+    this->historicoVacinacao.push_back(vacina);
+}
+
+void Bovino::exibirHistoricoVacinacao() const {
+    std::cout << "\n--- Historico de Vacinacao | Bovino ID: " << this->id << " ---" << std::endl;
+
+    if (this->historicoVacinacao.empty()) {
+        std::cout << "Nenhuma vacina registrada para este animal." << std::endl;
+        std::cout << "--------------------------------------------------------\n";
+        return;
+    }
+
+    for (const auto& vacina : this->historicoVacinacao) {
+        vacina.exibirDetalhes();
+    }
+    
+    std::cout << "--------------------------------------------------------\n";
 }
